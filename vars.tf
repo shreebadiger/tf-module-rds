51111ba3d7b0dac9ff8d286118bf1eb187f10c35
shrinivas <shinivassb16@gmail.com>
2024-01-19 23:29:38 +0530

    updated

--- vars.tf
+++ vars.tf
@@ -1,11 +1,11 @@
-variable "rds_allocated_storage"{}
-variable "rds_engine"{}
-variable "rds_engine_version"{}
-variable "rds_instance_class"{}
-variable "env"{}
-variable "tags"{}
-variable "subnets" {}
-variable "vpc_id" {}
-variable "sg_cidrs" {}
-variable "kms" {}
-variable "parameter_group_family" {}
+    variable "allocated_storage"{}
+    variable "engine"{}
+    variable "engine_version"{}
+    variable "instance_class"{}
+    variable "env"{}
+    variable "tags"{}
+    variable "subnets"{}
+    variable "vpc_id"{}
+    variable "sg_cidrs"{}
+    variable "kms"{}
+    variable "parameter_group_family"{}
